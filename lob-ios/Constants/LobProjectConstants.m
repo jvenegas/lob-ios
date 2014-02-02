6ea9f21622cf97ad85c6e02346aa1fbb48bdd5e3
Javier Venegas <venegas10@gmail.com>
2014-02-02 11:21:44 -0800

    Static removed from Constants

--- lob-ios/Constants/LobProjectConstants.m
+++ lob-ios/Constants/LobProjectConstants.m
@@ -8,33 +8,33 @@
 //
 
 //HTTP Types
-static NSString *const HTTP_Get = @"GET";
-static NSString *const HTTP_Post = @"POST";
-static NSString *const HTTP_Delete = @"DELETE";
-static NSString *const HTTP_Put = @"PUT";
+NSString *const HTTP_Get = @"GET";
+NSString *const HTTP_Post = @"POST";
+NSString *const HTTP_Delete = @"DELETE";
+NSString *const HTTP_Put = @"PUT";
 
 //URL Strings
-static NSString *const URLStr_Base = @"https://api.lob.com/v1";
+NSString *const URLStr_Base = @"https://api.lob.com/v1";
 
-static NSString *const URLStr_Addresses = @"/addresses";
-static NSString *const URLStr_BankAccounts = @"/bank_accounts";
-static NSString *const URLStr_Checks = @"/checks";
-static NSString *const URLStr_Countries = @"/countries";
-static NSString *const URLStr_Jobs = @"/jobs";
-static NSString *const URLStr_Objects = @"/objects";
-static NSString *const URLStr_Packagings = @"/packagings";
-static NSString *const URLStr_Postcards = @"/postcards";
-static NSString *const URLStr_Services = @"/services";
-static NSString *const URLStr_Settings = @"/settings";
-static NSString *const URLStr_States = @"/states";
-static NSString *const URLStr_Verify = @"/verify";
+NSString *const URLStr_Addresses = @"/addresses";
+NSString *const URLStr_BankAccounts = @"/bank_accounts";
+NSString *const URLStr_Checks = @"/checks";
+NSString *const URLStr_Countries = @"/countries";
+NSString *const URLStr_Jobs = @"/jobs";
+NSString *const URLStr_Objects = @"/objects";
+NSString *const URLStr_Packagings = @"/packagings";
+NSString *const URLStr_Postcards = @"/postcards";
+NSString *const URLStr_Services = @"/services";
+NSString *const URLStr_Settings = @"/settings";
+NSString *const URLStr_States = @"/states";
+NSString *const URLStr_Verify = @"/verify";
 
-static NSString *const URLStr_Addresses_ID = @"/addresses/%@";
-static NSString *const URLStr_BankAccounts_ID = @"/bank_accounts/%@";
-static NSString *const URLStr_Checks_ID = @"/checks/%@";
-static NSString *const URLStr_Jobs_ID = @"/jobs/%@";
-static NSString *const URLStr_Objects_ID = @"/objects/%@";
-static NSString *const URLStr_Packagings_ID = @"/packagings/%@";
-static NSString *const URLStr_Postcards_ID = @"/postcards/%@";
-static NSString *const URLStr_Services_ID = @"/services/%@";
-static NSString *const URLStr_Settings_ID = @"/settings/%@";
+NSString *const URLStr_Addresses_ID = @"/addresses/%@";
+NSString *const URLStr_BankAccounts_ID = @"/bank_accounts/%@";
+NSString *const URLStr_Checks_ID = @"/checks/%@";
+NSString *const URLStr_Jobs_ID = @"/jobs/%@";
+NSString *const URLStr_Objects_ID = @"/objects/%@";
+NSString *const URLStr_Packagings_ID = @"/packagings/%@";
+NSString *const URLStr_Postcards_ID = @"/postcards/%@";
+NSString *const URLStr_Services_ID = @"/services/%@";
+NSString *const URLStr_Settings_ID = @"/settings/%@";
